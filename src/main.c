2d936aefe342e5b9b6397780020a40d6a2fabff5
dacortes <dacortes@student.42barcelona.com>
2023-05-05 17:06:32 +0200

    test path

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 17:09:04 by dacortes          #+#    #+#             */
-/*   Updated: 2023/05/05 15:11:46 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/05/05 17:02:37 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,9 +39,7 @@
 
 int	main(int ac, char **av, char **env)
 {
-	// t_f_com parse;
-	// char	*command;
-	(void)env;
+	t_f_com parse;
 	char	**num_com;
 	char	*infile;
 	char	*outfile;
@@ -54,17 +52,36 @@ int	main(int ac, char **av, char **env)
 		exit (ERROR);
 	}
 	num_com = ft_calloc(sizeof(char *), ac);
-	if (num_com)
+	if (!num_com)
 	{
 		perror(E_MEMO);
 		return (ERROR);
 	}
+	ft_printf(B"PATH=%s\n"E, parse.path);
 	i = 0;
-	while (av[i])
+	while (av[i++])
+		num_com[i] = av[i];
+	infile = num_com[1];
+	outfile = num_com[ac - 1];
+	i = -1;
+	while (num_com[i++] && i < (ac - 2))
 	{
-		av[i]
+		command = ft_strjoin("/", num_com[i]);
+		if (!command)
+		{
+			perror(E_MEMO);
+			exit (ERROR);
+		}
+		if (!find_command(command, env, &parse) && !check_perissions(&parse))
+		{
+			free(command);
+			perror(E_PATH);
+			return (ERROR);
+		}
+		free(command);
 	}
-	
+	ft_printf(B"infile :%s\n"E, infile);
+	ft_printf(B"outfile :%s\n"E, outfile);
 	free(num_com);
 	return (SUCCESS);
 }
